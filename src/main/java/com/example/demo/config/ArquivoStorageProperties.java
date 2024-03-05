7f9517f5ad2d5fb9b74d25dd7e826fd69f6f65f0
eumarcelino <diegomarcelinoo@aluno.ueg.br>
2024-03-05 08:52:49 -0300

    arquivos

---
+++ src/main/java/com/example/demo/config/ArquivoStorageProperties.java
@@ -0,0 +1,13 @@
+package com.example.demo.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class ArquivoStorageProperties {
+
+    @Value("${arquivo.uploadDir}")
+    private String uploadDir;
+}
--- src/main/java/com/example/demo/service/ArquivoService.java
+++ src/main/java/com/example/demo/service/ArquivoService.java
@@ -1,2 +1,27 @@
-package com.example.demo.service;public class ArquivoService {
+package com.example.demo.service;
+
+import com.example.demo.config.ArquivoStorageProperties;
+import exception.UploadArquivoException;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class ArquivoService {
+
+    private final Path fileStorageLocation;
+
+    public  ArquivoService(ArquivoStorageProperties fileStorageProperties) {
+        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir())
+                .toAbsolutePath() .normalize();
+
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (IOException e) {
+            throw new UploadArquivoException("Algo deu errado ao tentar criar a pasta", e);
+        }
+    }
 }
