26d391a6162bf383f0be9c091641cb9672573543
eumarcelino <diegomarcelinoo@aluno.ueg.br>
2024-03-12 20:22:21 -0300

    finalizando controler

---
+++ src/main/java/com/example/demo/controller/ArquivoController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Arquivo;
+import com.example.demo.service.ArquivoService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/arquivos")
+@AllArgsConstructor
+
+public class ArquivoController {
+    private ArquivoService service;
+
+    @PostMapping("/upload")
+    public Arquivo uploadArquivo(@RequestParam ("file") MultipartFile file) {
+        String nomeArquivo = service.salvarArquivo(file);
+
+        String caminhoArquivo = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("E:/teste/downloaArquivo")
+                .path(nomeArquivo)
+                .toUriString();
+
+        return  new Arquivo(nomeArquivo, caminhoArquivo,file.getContentType(), file.getSize());
+    }
+
+    @GetMapping ("/downloadArquivo/{nomeArquivo}")
+    public ResponseEntity<Resource> downloadArquivo (String nomeArquivo, HttpServletRequest request) {
+
+        Resource resource = service.carregarArquivo (nomeArquivo);
+
+        String contentType = service.getContentType(request, resource);
+
+        return ResponseEntity.ok ()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+}
--- src/main/java/com/example/demo/service/ArquivoService.java
+++ src/main/java/com/example/demo/service/ArquivoService.java
@@ -1,15 +1,24 @@
 package com.example.demo.service;
 
 import com.example.demo.config.ArquivoStorageProperties;
+import exception.ArquivoNaoEncontradoException;
 import exception.UploadArquivoException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
+@Slf4j
 public class ArquivoService {
 
     private final Path fileStorageLocation;
@@ -23,5 +32,49 @@ public class ArquivoService {
         } catch (IOException e) {
             throw new UploadArquivoException("Algo deu errado ao tentar criar a pasta", e);
         }
+    }
+
+    public  String salvarArquivo (MultipartFile file) {
+        String nomeArquivo = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            if(nomeArquivo.contains("..")) {
+                throw new UploadArquivoException("Arquivo invalido");
+            }
+            Path targetLocation = this.fileStorageLocation.resolve(nomeArquivo);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+            return  nomeArquivo;
+        } catch ( IOException e) {
+            throw new UploadArquivoException("Erro ao tentar salvar o arquivo", e);
+        }
+    }
+
+    public Resource carregarArquivo (String nomeArquivo){
+        try {
+            Path filePath = this.fileStorageLocation.resolve (nomeArquivo) .normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new ArquivoNaoEncontradoException("arquivo nao encontrado");
+            }
+        } catch (Exception e) {
+            throw new ArquivoNaoEncontradoException("Arquivo nao encontrado");
+        }
+    }
+
+    public String getContentType (HttpServletRequest request, Resource resource){
+        String contentType = null;
+        try {
+            contentType = request.getServletContext(). getMimeType(resource.getFile(). getAbsolutePath() );
+        } catch (Exception e) {
+            log.error ("Nao foi possivel determinar o tipo de arquivo");
+        }
+
+        if(contentType ==null) {
+            contentType = "aplication/octet-stream";
+        }
+        return contentType;
     }
 }
